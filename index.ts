4082f067682b7e28901bd503aba8aebe010ab4a6
Esben von Buchwald <esben@vonbuchwald.dk>
2020-10-16 15:34:51 +0200

    Add index.ts

---
+++ index.ts
@@ -0,0 +1,6 @@
+export * from "./types/general";
+export * from "./types/record";
+export * from "./types/user";
+export * from "./types/case";
+export * from "./types/options";
+export * from "./types/options";
