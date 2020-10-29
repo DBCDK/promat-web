a207dfd4b2694bab63c6a9537ae31824ff059bb8
Ditlev Tøjner (konsdito) <ditlev@lundogbendsen.dk>
2020-10-29 13:53:47 +0100

    added init client react project files

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import './App.css'
+import Login from './components/Login'
+
+const NavLinks = [
+//   {to:"/overview",label:"Overblik", component:() => Overview},
+//   {to:"/create-case",label:"Opret sag", component:() => CreateCase},
+  // {to:"/create-case/:id",label:"Opret sag", component:() => CreateCaseWithMaterial},
+  // {to:"/find",label:"Find sag",component:() => SearchCase},
+  {to:"/",label:"Promat", component:() => Login},
+]
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div id="promat-app">
+      
+      <Switch>
+        {
+          NavLinks.map((link) => (
+          <Route path={link.to}>{React.createElement(link.component())}</Route>
+          ))
+        }
+      </Switch>
+
+      </div>
+    </BrowserRouter>
+  );
+}
