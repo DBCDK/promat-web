1307934bb7f8d022545d33eadcc07d4804c55715
Ditlev Tøjner (konsdito) <ditlev@lundogbendsen.dk>
2020-10-29 14:12:18 +0100

    added mockdata (bkms,records,reviewers,subjectAreas)

---
+++ mockdata/bkms.js
@@ -0,0 +1,18 @@
+export default {
+    bkm1: "Begrundelse for udeladelse af BKM",
+    bkm2: "Kun interesse for en snæver målgruppe",
+    bkm3: "For højt niveau i forhold til bibliotekernes målgrupper",
+    bkm4: "Kun af lokal interesse",
+    bkm5: "Privat karakter (ikke almen interesse)",
+    bkm6: "Bogens fysiske udstyr er ikke biblioteksegnet",
+    bkm7: "Manglende faglig kvalitet",
+    bkm8: "Mangelfuld redigering",
+    bkm9: "Sproglige fejl",
+    bkm10: "Faktuelle fejl",
+    bkm11: "Uoverensstemmelse mellem indhold og målgruppe",
+    bkm12: "Manglende litterær kvalitet",
+    bkm13: "Illustrationerne er af for ringe kvalitet",
+    bkm14: "Historien/plottet hænger ikke sammen",
+    bkm15: "Unuancerede personkarakteristikker",
+    bkm16: "Andet:"
+  }
---
+++ mockdata/records.js
@@ -0,0 +1,85 @@
+export default {
+    data: [
+        {
+            title: "Doktor kold og eventyrspejlet",
+            type: "Bog",
+            mediaType: "Bog",
+            status: "Klar til oprettelse",
+            author: "Isak Dinesen",
+            faust: "54330022",
+            weekCode: "25"
+        },
+        {
+            title: "Doktor kold og eventyrspejlet",
+            type: "Bog",
+            mediaType: "Bog",
+            status: "Klar til oprettelse",
+            author: "–",
+            faust: "54330022",
+            weekCode: "25"
+        },
+        {
+            title: "Doktor kold og eventyrspejlet",
+            type: "Bog",
+            mediaType: "Bog",
+            status: "Klar til oprettelse",
+            author: "–",
+            faust: "54330022",
+            weekCode: "25"
+        },
+        {
+            title: "Doktor kold og eventyrspejlet",
+            type: "Bog",
+            mediaType: "Bog",
+            status: "Klar til oprettelse",
+            author: "–",
+            faust: "54330022",
+            weekCode: "25"
+        },
+        {
+            title: "Doktor kold og eventyrspejlet",
+            type: "Bog",
+            mediaType: "Bog",
+            status: "Klar til oprettelse",
+            author: "–",
+            faust: "54330022",
+            weekCode: "25"
+        },
+        {
+            title: "Doktor kold og eventyrspejlet",
+            type: "Bog",
+            mediaType: "Bog",
+            status: "Klar til oprettelse",
+            author: "–",
+            faust: "54330022",
+            weekCode: "25"
+        },
+        {
+            title: "Doktor kold og eventyrspejlet",
+            type: "Bog",
+            mediaType: "Bog",
+            status: "Klar til oprettelse",
+            author: "–",
+            faust: "54330022",
+            weekCode: "25"
+        },
+        {
+            title: "Doktor kold og eventyrspejlet",
+            type: "Bog",
+            mediaType: "Bog",
+            status: "Klar til oprettelse",
+            author: "–",
+            faust: "54330022",
+            weekCode: "25"
+        },
+        {
+            title: "Doktor kold og eventyrspejlet",
+            type: "Bog",
+            mediaType: "Bog",
+            status: "Klar til oprettelse",
+            author: "–",
+            faust: "54330022",
+            weekCode: "25"
+        },
+    ]
+}
